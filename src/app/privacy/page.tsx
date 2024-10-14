53fea526066f9088c56624c0b209762dcb2b904a
Mitchell Oliarny <mitchelloliarny@gmail.com>
2024-10-14 17:42:20 -0600

    added styling to tos privacy pages

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,16 +1,9 @@
-"use server";
+
 import React, { } from "react";
 //import { metadata } from "../layout";
 import type { Metadata } from "next";
 import PrivacyPolicy from "../components/Legal/PrivacyPolicy";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "ESC || Privacy Policy",
-    description:
-      "Esports Clubs' Privacy Policy",
-  };
-}
 
 export default async function Privacy({
 
@@ -20,7 +13,9 @@ export default async function Privacy({
 
   return (
     <>
-      <PrivacyPolicy />
+      <div className="w-[80%] mx-auto my-10">
+        <PrivacyPolicy />
+      </div>
     </>
   );
 }
--- src/app/tos/page.tsx
+++ src/app/tos/page.tsx
@@ -1,16 +1,7 @@
-"use server";
 import React, { } from "react";
 //import { metadata } from "../layout";
 import type { Metadata } from "next";
 import Terms from "../components/Legal/Terms";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "ESC || TOS",
-    description:
-      "Esports Clubs' Terms of Service",
-  };
-}
 
 export default async function TOS({
 
@@ -20,7 +11,9 @@ export default async function TOS({
 
   return (
     <>
-      <Terms />
+      <div className="w-[80%] mx-auto my-10">
+        <Terms />
+      </div>
     </>
   );
 }
