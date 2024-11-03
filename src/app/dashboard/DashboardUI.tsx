bbefac525aa5a1e6d65617bbf8387b7dbd42f66b
Mitchellol <mitchelloliarny@gmail.com>
2024-11-03 15:14:36 -0700

    fixed error

--- src/app/dashboard/DashboardUI.tsx
+++ src/app/dashboard/DashboardUI.tsx
@@ -103,6 +103,7 @@ export default function Header({
             agentGames={agentGames}
             topAgents={newTopAgents}
             valAverage={valAverages}
+            maps={valMaps}
           />
         );
       case "medals":
